fa0961665bbb349d4f1cbdfdba2d01a4f90e0b66
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-04-26 08:52:06 -0300

    adding cache file listing option

--- homework01/tcp_client.py
+++ homework01/tcp_client.py
@@ -7,12 +7,21 @@ BUFFER_SIZE = 1024
 # criando socket
 s = socket.socket()
 
-host = sys.argv[1]            # host
-port = int(sys.argv[2])       # port
-res = sys.argv[3]             # file_name
-save_directory = sys.argv[4]  # path to save file
-if save_directory[len(save_directory) - 1] != '/': save_directory += '/' #add / ao final do diretorio de salvamento
-if save_directory == '.' or save_directory == './' : save_directory = ''
+host = 'localhost'
+port = 9089
+res = 'list'
+
+if len(sys.argv) == 5:
+    host = sys.argv[1]            # host
+    port = int(sys.argv[2])       # port
+    res = sys.argv[3]             # file_name
+    save_directory = sys.argv[4]  # path to save file
+    if save_directory[len(save_directory) - 1] != '/': save_directory += '/' #add / ao final do diretorio de salvamento
+    if save_directory == '.' or save_directory == './' : save_directory = ''
+else:
+    host = sys.argv[1]          # host
+    port = int(sys.argv[2])     # port
+    res = sys.argv[3]           # list-operation
 
 # conectando o socket com o servidor
 s.connect((host, port))
@@ -23,20 +32,27 @@ msg = pickle.dumps((res))
 # enviando dados
 s.sendall(msg)
 
-# caminho para salvar o arquivo
-path_to_save = save_directory + res
-# criando buffer para armazenar a resposta do servidor
-buffer = s.recv(BUFFER_SIZE)
-# carregando buffer com os dados enviados do servidor
-res = pickle.loads(buffer)
-if res:
-    with open(path_to_save, 'wb') as file:
-        print('downloading', res)
-        package = s.recv(BUFFER_SIZE)
-        while package:
-            file.write(package) # salvando o arquivo no disco
-            package = s.recv(BUFFER_SIZE)
-        file.close()
-    print("file '%s' saved!" % (res))
+if res == 'list':
+    data = s.recv(BUFFER_SIZE)
+    while data:
+        res = pickle.loads(data)
+        print(res)
+        data = s.recv(BUFFER_SIZE)
 else:
-    print("file '%s' not found" % (res))
+    # caminho para salvar o arquivo
+    path_to_save = save_directory + res
+    # criando buffer para armazenar a resposta do servidor
+    buffer = s.recv(BUFFER_SIZE)
+    # carregando buffer com os dados enviados do servidor
+    res = pickle.loads(buffer)
+    if res:
+        with open(path_to_save, 'wb') as file:
+            print('downloading', res)
+            package = s.recv(BUFFER_SIZE)
+            while package:
+                file.write(package) # salvando o arquivo no disco
+                package = s.recv(BUFFER_SIZE)
+            file.close()
+        print("file '%s' saved!" % (res))
+    else:
+        print("file '%s' not found" % (res))
--- homework01/tcp_thread.py
+++ homework01/tcp_thread.py
@@ -15,92 +15,103 @@ def threaded(c, addr, directory):
     data = c.recv(BUFFER_SIZE)
     # deserializando a request
     res = pickle.loads(data)
-
-    print("[port: %s] client is requesting file '%s'" % (
-        addr[1], res))
-
-    # check if file exists in the directory or cache
-    if file_control.isExist(directory + res) or res in cache_control.cache:
-        # checks if file is larger than the cache
-        if file_control.getFile_size(directory + res) > cache_control.cache_size and res not in cache_control.cache:
-            print("[port: %s] sending file '%s' to client" % (addr[1], res))
-            # prepares client to receive file
-            package = pickle.dumps(True)
-            c.send(package)
-
-            # serialization of the file for sending
-            with open(directory + res, 'rb') as f:
-                package = f.read(BUFFER_SIZE)
-                while package:
-                    # sending file packages
-                    c.send(package)
-                    package = f.read(BUFFER_SIZE)
-                sleep(1)
-                f.close()
-
-            print("[port: %s] sent file '%s'" % (
-                        addr[1], res))
+    if res == 'list':
+        print("[port: %s] checking files on cache" % (addr[1]))
+        if len(cache_control.cache) == 0:
+            c.send(pickle.dumps('cache is empty'))
         else:
-            # check if file exists in cache
-            if res not in cache_control.cache:
+            package = '\nFiles on cache:\n\n'
+            for file in cache_control.cache.keys():
+                package += file + '\n'
+            c.send(pickle.dumps(package))
 
-                print("[port: %s] add file '%s' on cache" % (
-                    addr[1], res))
-                size_file = file_control.getFile_size(directory + res)
+            print("[port: %s] list files on cache sended to client" % (addr[1]))
+    else:
+        print("[port: %s] client is requesting file '%s'" % (
+            addr[1], res))
 
-                # free up cache space
-                if cache_control.cache_available < size_file:
-                    for key_file in list(cache_control.cache):
-                        # remove file from cache
-                        if not cache_control.cache[key_file][2]:
-                            print("[port: %s] remove a file '%s' by cache" % (
-                                addr[1], key_file))
-                            file_removed = cache_control.cache.pop(key_file)
-                            file_removed_size = file_removed[0]
-                            cache_control.cache_available += file_removed_size
+        # check if file exists in the directory or cache
+        if file_control.isExist(directory + res) or res in cache_control.cache:
+            # checks if file is larger than the cache
+            if file_control.getFile_size(directory + res) > cache_control.cache_size and res not in cache_control.cache:
+                print("[port: %s] sending file '%s' to client" % (addr[1], res))
+                # prepares client to receive file
+                package = pickle.dumps(True)
+                c.send(package)
 
-                # serialize file
-                packages = []
+                # serialization of the file for sending
                 with open(directory + res, 'rb') as f:
                     package = f.read(BUFFER_SIZE)
                     while package:
-                        packages.append(package)
+                        # sending file packages
+                        c.send(package)
                         package = f.read(BUFFER_SIZE)
                     sleep(1)
                     f.close()
 
-                # add file to cache
-                cache_control.cache[res] = [size_file, packages, True]
-                cache_control.cache_available -= size_file
+                print("[port: %s] sent file '%s'" % (
+                            addr[1], res))
+            else:
+                # check if file exists in cache
+                if res not in cache_control.cache:
 
-                print("[port: %s] file '%s' on cache" % (addr[1], res))
+                    print("[port: %s] add file '%s' on cache" % (
+                        addr[1], res))
+                    size_file = file_control.getFile_size(directory + res)
 
-            # query cache file to be sent
-            file_in_cache = cache_control.cache[res]
-            size_file = file_in_cache[0]
-            packages = file_in_cache[1]
+                    # free up cache space
+                    if cache_control.cache_available < size_file:
+                        for key_file in list(cache_control.cache):
+                            # remove file from cache
+                            if not cache_control.cache[key_file][2]:
+                                print("[port: %s] remove a file '%s' by cache" % (
+                                    addr[1], key_file))
+                                file_removed = cache_control.cache.pop(key_file)
+                                file_removed_size = file_removed[0]
+                                cache_control.cache_available += file_removed_size
 
-            # flush file from cache
-            cache_control.cache[res][2] = False
+                    # serialize file
+                    packages = []
+                    with open(directory + res, 'rb') as f:
+                        package = f.read(BUFFER_SIZE)
+                        while package:
+                            packages.append(package)
+                            package = f.read(BUFFER_SIZE)
+                        sleep(1)
+                        f.close()
 
-            # -------
-            # sending through the cache
-            print("[port: %s] sending file '%s' to client through the cache" % (
-                addr[1], res))
-            package = pickle.dumps(True)
-            c.send(package)
+                    # add file to cache
+                    cache_control.cache[res] = [size_file, packages, True]
+                    cache_control.cache_available -= size_file
 
-            for package in packages:
+                    print("[port: %s] file '%s' on cache" % (addr[1], res))
+
+                # query cache file to be sent
+                file_in_cache = cache_control.cache[res]
+                size_file = file_in_cache[0]
+                packages = file_in_cache[1]
+
+                # flush file from cache
+                cache_control.cache[res][2] = False
+
+                # -------
+                # sending through the cache
+                print("[port: %s] sending file '%s' to client through the cache" % (
+                    addr[1], res))
+                package = pickle.dumps(True)
                 c.send(package)
-            sleep(1)
-            print("[port: %s] sent file '%s' to client" % (
-                addr[1], res))
-    # if file does not exist
-    else:
-        print("[port: %s] file '%s' not found" % (addr[1], res))
-        # warns that file does not exist
-        package = pickle.dumps(False)
-        c.send(package)
+
+                for package in packages:
+                    c.send(package)
+                sleep(1)
+                print("[port: %s] sent file '%s' to client" % (
+                    addr[1], res))
+        # if file does not exist
+        else:
+            print("[port: %s] file '%s' not found" % (addr[1], res))
+            # warns that file does not exist
+            package = pickle.dumps(False)
+            c.send(package)
 
     print("[port: %s] closed connection with client" % (addr[1]))
     c.close()
