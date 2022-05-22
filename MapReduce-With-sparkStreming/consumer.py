7cf4631d929c6fd80ecfd9f6b36b9ae51bdf2fe0
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-05-22 16:47:23 -0300

    nova tentativa de comunicação socket, desta vez enviando llinha por linha

--- MapReduce-With-sparkStreming/consumer.py
+++ MapReduce-With-sparkStreming/consumer.py
@@ -2,7 +2,7 @@ import findspark
 findspark.init()
 
 import time
-import pickle
+import json
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark import StorageLevel
@@ -12,28 +12,18 @@ streamingSocket = StreamingContext(socket, 30)
 
 
 socketStreaming = streamingSocket.socketTextStream("127.0.0.1",56789)
-lines = socketStreaming.flatMap(lambda line: pickle.loads(line))
+
+linesJSON = socketStreaming.map(lambda line: json.loads(line))
+linesJSON.pprint()
+
+movieRatings = linesJSON.map(lambda x: (int(x.split('::')[1]), [float(x.split('::')[2]),1]))
+
+totalOfMovieRatings = movieRatings.reduceByKey(lambda x, y: [x[0] + y[0],x[1]+y[1]])
+linesJSON.map(lambda line: np.fromstring(line,np.int8))
 
 print(lines)
 lines.pprint()
-
-lines.map(lambda line: np.fromstring(line,np.int8))
-
-print(lines)
-lines.pprint()
-
-# movieRatings = interactions.map(lambda x: (int(x.split('::')[1]), [float(x.split('::')[2]),1]))
-#     totalOfMovieRatings = movieRatings.reduceByKey(lambda x, y: [x[0] + y[0],x[1]+y[1]])
-
-# res = pairs.reduceByKey(lambda a, b: a+b)
-
-# res.pprint()
-
-# ratings = ssc.textFileStream("../database/").map
-# ratings.pprint()
 
 streamingSocket.start()
 
 streamingSocket.awaitTermination()
-# WARN RandomBlockReplicationPolicy: Expecting 1 replicas with only 0 peer/s.
-# WARN BlockManager: Block input-0-1653087338600 replicated to only 0 peer(s) instead of 1 peers
--- MapReduce-With-sparkStreming/produtor.py
+++ MapReduce-With-sparkStreming/produtor.py
@@ -9,29 +9,18 @@ lines = open("ratings.dat").read().splitlines()
 hostip = '127.0.0.1'
 portno = 56789
 
-#listener need to be started before!
-#try: netcat -lkp 56789
-#before you start with spark streaming
-
 soc = socket.socket()
-
-
-print('antes connect')
 soc.bind((hostip, portno))
-
-print('------')
 
 soc.listen(1)
 
 (connection, a) = soc.accept()
-print('passou connection')
 
 while True:
     print('Enviando linhas')
-    myline = np.random.choice(lines,200)
-    #print(myline)
-    #serialized_data = pickle.dumps(myline, protocol=2)
-    myline2 = np.array2string(myline)
-    serialized_data = pickle.dumps((myline2))
-    connection.send(serialized_data)
+    lines = np.random.choice(lines,200)
+    for line in lines:
+        lineJSON = json.dumps(line)
+        connection.send(bytes((lineJSON+'\n').encode('utf-8')))
+    print("200 linhas enviadas")
     time.sleep(30)
