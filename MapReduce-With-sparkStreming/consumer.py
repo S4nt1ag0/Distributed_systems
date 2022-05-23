fda64a02413d943cc0272c098fec451a4b5044d0
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-05-22 21:25:14 -0300

    tentando somente lendo o arquivo .bat

--- MapReduce-With-sparkStreming/consumer.py
+++ MapReduce-With-sparkStreming/consumer.py
@@ -1,30 +1,29 @@
 import findspark
 findspark.init()
 
-import time
 import json
-from pyspark import SparkContext
-from pyspark.conf import SparkConf
-from pyspark.streaming import StreamingContext
-from pyspark import StorageLevel
+from pyspark.sql import SparkSession
 
-conf = SparkConf()
-conf.setAppName('Demo').set("spark.executor.memory", "500M").set("spark.driver.memory", "500M")
+spark = SparkSession.builder.getOrCreate()
 
+def load_movie_names(filename):
+    movie_names={}
+    with open(filename, encoding= 'ISO-8859-1') as f:
+        for line in f:
+            tokens=line.split('::')
+            key=int(tokens[0])
+            movie_names[key]=tokens[1]
+    return movie_names
 
-socket = SparkContext(conf=conf)
-streamingSocket = StreamingContext(socket,45)
+movie_names=load_movie_names('movies.dat')
 
+lines=spark.sparkContext.textFile('ratings.dat')
 
-socketStreaming = streamingSocket.socketTextStream("127.0.0.1",56789)
-
-linesJSON = socketStreaming.map(lambda line: json.loads(line))
-linesJSON.pprint()
-
-movieRatings = linesJSON.map(lambda x: (int(x.split('::')[1]), [float(x.split('::')[2]),1]))
-
-print(movieRatings)
-
-streamingSocket.start()
-
-streamingSocket.awaitTermination()
+movieRatings = lines.map(lambda x: (int(x.split('::')[1]), [float(x.split('::')[2]),1]))
+totalOfMovieRatings = movieRatings.reduceByKey(lambda x, y: [x[0] + y[0],x[1]+y[1]])
+FinalMoviexRating=totalOfMovieRatings.mapValues(lambda x: x[0] / x[1])
+FinalRatingxMovie=FinalMoviexRating.map(lambda kv: (kv[1], kv[0]))
+sortedMovies = FinalRatingxMovie.sortByKey(False)
+results= sortedMovies.take(10)
+for result in results:
+    print(movie_names[result[1]],",", round(result[0], 2))
