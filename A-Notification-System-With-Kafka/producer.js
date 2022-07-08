42a4f4d9f4f39b349254eac345efd733eeb0aca3
Gustavo Santiago <43881387+S4nt1ag0@users.noreply.github.com>
2022-07-07 23:22:46 -0300

    update producer

--- A-Notification-System-With-Kafka/producer.js
+++ A-Notification-System-With-Kafka/producer.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const kafka = new Kafka({
   clientId: 'my-app',
-  brokers: ['ec2-35-172-229-58.compute-1.amazonaws.com:9092'],
+  brokers: ['ec2-18-208-190-1.compute-1.amazonaws.com:9092'],
 })
 
 const producer = kafka.producer()
