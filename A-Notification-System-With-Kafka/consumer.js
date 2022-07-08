2b0d11c85e96d29337650950d31f2ed9556fa79f
Gustavo Santiago <43881387+S4nt1ag0@users.noreply.github.com>
2022-07-08 00:36:52 -0300

    Update Consumer

--- A-Notification-System-With-Kafka/consumer.js
+++ A-Notification-System-With-Kafka/consumer.js
@@ -1,20 +1,41 @@
 const { Kafka } = require('kafkajs')
-
+const express = require('express')
+const bodyParser = require('body-parser')
 const kafka = new Kafka({
   clientId: 'my-app',
   brokers: ['ec2-18-208-190-1.compute-1.amazonaws.com:9092'],
 })
 
-
 const consumer = kafka.consumer({ groupId: 'test-group' })
 
-await consumer.connect()
-await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+const app = express()
 
-await consumer.run({
+const jsonParser = bodyParser.json()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+async function enviaNotificacao(data) {
+  await producer.send({
+    topic: data.topic,
+    messages: [
+      { value: data.message},
+    ],
+  })
+}
+
+
+app.listen(9000, async ()=>{
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'quickstart-event', fromBeginning: true })
+  await consumer.run({
   eachMessage: async ({ topic, partition, message }) => {
     console.log({
       value: message.value.toString(),
     })
   },
 })
+})
+
+
